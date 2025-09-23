d2195e8780b8ea040f909f3fe4606554fff988ee
bryarrow <220811010104@qhu.edu.cn>
2025-09-23 23:00:37 +0800

    chore: add a math test

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -5,25 +5,25 @@ import { ref } from 'vue'
 import DataPanel from '@/components/DataPanel.vue'
 import StylePanel from '@/components/StylePanel.vue'
 
-const sourceCode = '= Hello World!\nThis is a typst document compiled in browser.\n\n#lorem(900)'
+const sourceCode =
+  '= Hello World!\nThis is a typst document compiled in browser.\n\n$\na_b=sum^c\n$\n\n#lorem(900)'
 
 const artifact = ref(new Uint8Array(0))
 $typst.vector({ mainContent: sourceCode }).then((value) => {
   if (value) artifact.value = value
 })
-
 </script>
 
 <template>
   <el-splitter>
     <el-splitter-panel>
-      <DataPanel/>
+      <DataPanel />
     </el-splitter-panel>
     <el-splitter-panel>
       <TypstPreviewer :artifact="artifact" />
     </el-splitter-panel>
     <el-splitter-panel collapsible>
-      <StylePanel/>
+      <StylePanel />
     </el-splitter-panel>
   </el-splitter>
 </template>
