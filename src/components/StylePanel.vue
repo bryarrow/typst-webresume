4dcd77136a4f138e99a4efd060c2e499c6ad3d89
bryarrow <220811010104@qhu.edu.cn>
2025-09-23 22:56:06 +0800

    feat: [WIP]add data and style panel draft

---
+++ src/components/StylePanel.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="h-[98%] p-4 bg-[rgba(228,229,234,1)] dark:bg-[rgba(59,58,72,1)] mx-2 rounded">
+    <h1 class="font-bold text-2xl">Style</h1>
+    <p>Working in progress</p>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -2,6 +2,8 @@
 import { $typst } from '@myriaddreamin/typst.ts'
 import TypstPreviewer from '@/components/TypstPreviewer.vue'
 import { ref } from 'vue'
+import DataPanel from '@/components/DataPanel.vue'
+import StylePanel from '@/components/StylePanel.vue'
 
 const sourceCode = '= Hello World!\nThis is a typst document compiled in browser.\n\n#lorem(900)'
 
@@ -14,10 +16,14 @@ $typst.vector({ mainContent: sourceCode }).then((value) => {
 
 <template>
   <el-splitter>
-    <el-splitter-panel></el-splitter-panel>
     <el-splitter-panel>
-      <TypstPreviewer :artifact="artifact"></TypstPreviewer>
+      <DataPanel/>
     </el-splitter-panel>
-    <el-splitter-panel collapsible></el-splitter-panel>
+    <el-splitter-panel>
+      <TypstPreviewer :artifact="artifact" />
+    </el-splitter-panel>
+    <el-splitter-panel collapsible>
+      <StylePanel/>
+    </el-splitter-panel>
   </el-splitter>
 </template>
