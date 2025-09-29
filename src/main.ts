f655f9924d1789f0f90a1b8174caa4c9d460d7fe
bryarrow <220811010104@qhu.edu.cn>
2025-09-29 11:59:53 +0800

    chore: move typst init options setter to single file

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import ElementPlus from 'element-plus'
 // other libs
 import { createApp } from 'vue'
 import router from './router'
+import setTypst from '@/utils/typst-compiler/set-init-options'
 
 // the App component
 import App from './App.vue'
@@ -17,25 +18,4 @@ app.use(ElementPlus)
 
 app.mount('#app')
 
-import { $typst, loadFonts } from '@myriaddreamin/typst.ts'
-import pako from 'pako'
-
-$typst.setCompilerInitOptions({
-  beforeBuild: [
-    loadFonts([], {
-      assets: ['text', 'cjk'],
-      assetUrlPrefix: {
-        text: 'https://cdn.jsdmirror.com/gh/typst/typst-assets@v0.13.1/files/fonts/',
-        _: 'https://cdn.jsdmirror.com/gh/typst/typst-dev-assets@v0.13.1/files/fonts/',
-      },
-    }),
-  ],
-  getModule: async () => {
-    const buffer = await (
-      await fetch(
-        'https://cdn.jsdmirror.com/gh/bryarrow/typst-webresume@main/public/wasm/typst_ts_web_compiler_bg.wasm.gz',
-      )
-    ).arrayBuffer()
-    return pako.inflate(new Uint8Array(buffer)).buffer
-  },
-})
+setTypst()
