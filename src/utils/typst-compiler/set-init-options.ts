0322e1d1797cc792d6e4b746a5dea684c367aada
bryarrow <220811010104@qhu.edu.cn>
2025-09-29 17:28:00 +0800

    feat: prevent double set typst

--- src/utils/typst-compiler/set-init-options.ts
+++ src/utils/typst-compiler/set-init-options.ts
@@ -1,24 +1,29 @@
 import { $typst, loadFonts } from '@myriaddreamin/typst.ts'
 import pako from 'pako'
 
-export default function () {
-  $typst.setCompilerInitOptions({
-    beforeBuild: [
-      loadFonts([], {
-        assets: ['text', 'cjk'],
-        assetUrlPrefix: {
-          text: 'https://cdn.jsdmirror.com/gh/typst/typst-assets@v0.13.1/files/fonts/',
-          _: 'https://cdn.jsdmirror.com/gh/typst/typst-dev-assets@v0.13.1/files/fonts/',
-        },
-      }),
-    ],
-    getModule: async () => {
-      const buffer = await (
-        await fetch(
-          'https://cdn.jsdmirror.com/gh/bryarrow/typst-webresume@main/public/wasm/typst_ts_web_compiler_bg.wasm.gz',
-        )
-      ).arrayBuffer()
-      return pako.inflate(new Uint8Array(buffer)).buffer
-    },
-  })
+let isSet = false
+
+export default function setTypst() {
+  if (!isSet) {
+    $typst.setCompilerInitOptions({
+      beforeBuild: [
+        loadFonts([], {
+          assets: ['text', 'cjk'],
+          assetUrlPrefix: {
+            text: 'https://cdn.jsdmirror.com/gh/typst/typst-assets@v0.13.1/files/fonts/',
+            _: 'https://cdn.jsdmirror.com/gh/typst/typst-dev-assets@v0.13.1/files/fonts/',
+          },
+        }),
+      ],
+      getModule: async () => {
+        const buffer = await (
+          await fetch(
+            'https://cdn.jsdmirror.com/gh/bryarrow/typst-webresume@main/public/wasm/typst_ts_web_compiler_bg.wasm.gz',
+          )
+        ).arrayBuffer()
+        return pako.inflate(new Uint8Array(buffer)).buffer
+      },
+    })
+    isSet = true
+  }
 }
