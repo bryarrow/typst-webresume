115fd127dcdab00e125bbed0b6f7ff2e5316ef6c
bryarrow <220811010104@qhu.edu.cn>
2025-10-01 12:18:26 +0800

    feat: [WIP]add a temp home page for test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
+import { useProjectsStore } from '@/store/projects-store.ts'
+import router from '@/router'
 
+const projectsStore = useProjectsStore()
+const newProject = () => {
+  projectsStore.addProject(
+    `test-${Object.keys(projectsStore.allProjectsData).length + 1}`,
+    'typsume-cv-miku',
+  )
+}
 </script>
 
 <template>
   <div class="h-full">
-
+    <el-button
+      v-for="proj of projectsStore.allProjectsData"
+      :key="proj.id"
+      @click="router.push(`/resume/${proj.id}`)"
+    >
+      {{ proj.name }}
+    </el-button>
+    <el-button @click="newProject">New</el-button>
   </div>
 </template>
