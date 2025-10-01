5bdb51538a2f279f9c8948490369087debafff5a
bryarrow <220811010104@qhu.edu.cn>
2025-10-01 12:17:35 +0800

    feat: use projectStore as data source instead of test data

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,29 +1,42 @@
 <script setup lang="ts">
-import { $typst } from '@myriaddreamin/typst.ts'
-import TypstPreviewer from '@/components/TypstPreviewer.vue'
-import { onMounted,  ref } from 'vue'
-import DataPanel from '@/components/DataPanel.vue'
-import StylePanel from '@/components/StylePanel.vue'
-import { loadWithDefault } from '@/utils/template/load/template.ts'
+import { computed, ref, watch } from 'vue'
+import debounce from 'lodash.debounce'
 
-const artifact = ref<Uint8Array>(new Uint8Array())
+import DataPanel from '@/components/ResumePanels/DataPanel.vue'
+import TypstPreviewer from '@/components/ResumePanels/TypstPreviewer.vue'
+import StylePanel from '@/components/ResumePanels/StylePanel.vue'
+
+import { useProjectsStore } from '@/store/projects-store.ts'
+
 const isLoading = ref(true)
-
-onMounted(async () => {
-  const { templateMeta } = await loadWithDefault('typsume-cv-miku')
-  const vector = await $typst.vector({ root: '/', mainFilePath: templateMeta.main })
-  if (vector) {
-    artifact.value = vector
-  }
-  isLoading.value = false
+const projectStore = useProjectsStore()
+const artifact = ref<Uint8Array>(new Uint8Array(0))
+const data = computed({
+  get() {
+    return projectStore.activeProject?.data ?? null
+  },
+  set(val) {
+    if (projectStore.activeProject !== undefined) {
+      projectStore.activeProject.data = val
+    }
+  },
 })
 
+const updateArtifact = debounce(async () => {
+  if (!projectStore.activeProject) return
+  isLoading.value = true
+  const newVector = await projectStore.activeProject.compileVector()
+  artifact.value = newVector ?? artifact.value
+  isLoading.value = false
+}, 300)
+
+watch(()=>projectStore.activeProject?.data, updateArtifact, { immediate: true ,deep: true })
 </script>
 
 <template>
   <el-splitter>
     <el-splitter-panel>
-      <DataPanel />
+      <DataPanel v-model:data="data" />
     </el-splitter-panel>
     <el-splitter-panel>
       <TypstPreviewer :artifact="artifact" :is-loading="isLoading" />
